573e5fe615e983ecc76584031cbc85148247ec0e
AnkhHatar <116153289+AnkhHatar@users.noreply.github.com>
2022-11-03 00:28:10 +0900

    Update Отрезок и его длина.java

--- Отрезок и его длина.java
+++ Отрезок и его длина.java
@@ -35,3 +35,42 @@ public class Segment{                                           //Определ
      System.out.printf("Segment d is on (a (%.2f,%.2f), b (%.2f,%.2f))\n", a.x,a.y,b.x,b.y);  
     }
 }  
+
+
+
+//Второй вариант исполнения
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Point a = new Point (-6,5);
+        Point b = new Point (0,2);
+        Segment c = new Segment (a,b);
+        c.printSegment();
+        c.calcSegment();
+    }
+}
+public class Point {
+    public double x;
+    public double y;
+    public Point (double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Segment {
+    public Segment (Point a, Point b){
+    this.a = a;
+    this.b = b;
+    }
+    public Point a;
+    public Point b;
+    public double segment;
+    public void printSegment(){
+        System.out.printf("Segment is on ( a(%.1f, %.1f), b(%.1f, %.1f)\n", a.x, a.y, b.x,b.y);
+    }
+    public void calcSegment(){
+        segment = Math.sqrt(((b.x-a.x)*(b.x-a.x)) +((b.y-a.y) * ((b.y-a.y))));
+        System.out.println(segment);
+    }
+}
